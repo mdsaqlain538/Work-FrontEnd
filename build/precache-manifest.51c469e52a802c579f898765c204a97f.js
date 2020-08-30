b9a61b2ab0708ad6c4f6fb96e0238f6176a10fd0
mdsaqlain538 <mdsaqlain538@gmail.com>
2020-08-30 07:56:23 +0530

    Netlfy.toml

---
+++ build/precache-manifest.51c469e52a802c579f898765c204a97f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4229e11facc74445804d7ded415b4a04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c7cca494d3741c7aa3",
+    "url": "/static/css/main.f8c66062.chunk.css"
+  },
+  {
+    "revision": "49728493876059028ec3",
+    "url": "/static/js/2.18ad385d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.18ad385d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c7cca494d3741c7aa3",
+    "url": "/static/js/main.f9b2c0b4.chunk.js"
+  },
+  {
+    "revision": "e8d23b230335e593b387",
+    "url": "/static/js/runtime-main.744fb23b.js"
+  },
+  {
+    "revision": "4164a914475ae5142caf0bc3e05f4aac",
+    "url": "/static/media/duo.4164a914.jpeg"
+  },
+  {
+    "revision": "b4dc10a2590bd46d4e2d4baf66202286",
+    "url": "/static/media/pp.b4dc10a2.jpeg"
+  },
+  {
+    "revision": "7586c820703d8e8084f554ca82fd5cf5",
+    "url": "/static/media/solo.7586c820.jpg"
+  },
+  {
+    "revision": "c84dac184b6451627ff8d2f1aa5c85e0",
+    "url": "/static/media/squad.c84dac18.jpeg"
+  }
+]);
